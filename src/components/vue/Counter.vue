12d7e419be16509027e7b6a64cff49fe83d3bc3e
julian <jaguerreropa@ufpso.edu.co>
2024-05-15 13:19:10 -0500

    v1 login

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {ref} from 'vue'
+    let count=ref(0); 
+    function addvalue() {
+        count.value++; 
+        console.log(count)
+    }
+
+
+</script>
+
+<template>
+    <div @click="addvalue" v-on:click="" class="bg-purple-700 text-white py-2 px-4 rounded  inline-block hover:cursor-pointer transition-colors">
+        ¡Presiona aquí! {{ count }}
+    </div>
+</template>
